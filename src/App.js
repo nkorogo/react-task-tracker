3e8fb41048b726e296ecf1c3d089d0b7f674b768
Magige <44432056+nkorogo@users.noreply.github.com>
2021-03-03 12:18:07 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header title = "Hello props"/>
+    </div>
+
+  );
+}
+
+export default App;
